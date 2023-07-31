8e13e09ada8338bccba34131d7159a182680d33b
Kavinkumar1998 <kavinajith412@gmail.com>
2023-08-01 00:21:11 +0530

    trailer and info page updated

--- src/Components/MovieInfo/MovieInfo.js
+++ src/Components/MovieInfo/MovieInfo.js
@@ -5,7 +5,9 @@ import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMovieById } from '../MovieRedux/Slice';
 import { useHistory } from 'react-router-dom'
-import { LinearProgress } from '@mui/material';
+import { Button, LinearProgress } from '@mui/material';
+import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
+import StarIcon from '@mui/icons-material/Star';
 
 export  const MovieInfo = () => {
 
@@ -34,11 +36,30 @@ export  const MovieInfo = () => {
 <img  src={selectedMovie.image} className="movieinfo-img" alt={selectedMovie.title}/>
      </div>
      <div className="MovieInfo-right">
-
-     </div>
- </div>
-   
+<div className="MovieInfo-title">{selectedMovie.title}</div>
+<span className="MovieInfo-details"><span ><StarIcon className='icon' sx={{color:"yellow"}}/></span>{selectedMovie.imDbRating}</span>
+<span className="MovieInfo-details"><span ><AccessTimeOutlinedIcon className='icon'/></span>{selectedMovie.runtimeStr}</span>
+<span className="MovieInfo-details">{selectedMovie.contentRating}-Rated</span>
+<span className="MovieInfo-details">4K</span>
+<span className="MovieInfo-details">Doloby-Atoms</span>
+<p className="MovieInfo-details">{selectedMovie.plot}</p>
+<div className="MovieInfo-details">Genre : {selectedMovie.genres}</div>
+<div className="MovieInfo-details">Director : <br/>{selectedMovie.directors}</div>
+<div className="MovieInfo-details">Stars : <br/>{selectedMovie.stars && selectedMovie.stars.join(" , ")}</div>
+<div className="MovieInfo-details">Box Office  <br/>{selectedMovie.boxOffice && selectedMovie.boxOffice &&(
+  <div className="MovieInfo-details">Budget :{selectedMovie.boxOffice.budget}</div>
+)}</div>
+<Button variant="contained" color="success" onClick={()=>{history.push(``)}}>Book Ticket</Button>
+     </div>  
+     <div className="trailer-backdrops">
+     {selectedMovie.trailer && selectedMovie.trailer.linkEmbed &&(
+         <iframe className="trailer" title ={selectedMovie.trailer.title} src={selectedMovie.trailer.linkEmbed}></iframe>
+      )}
+   </div>
+ </div> 
+ 
   </div>
+  
 )}
 </div>
     </div>
